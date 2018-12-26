ec6c8a2bc9492c7afafeea26c84c942551004991
Nick O'Ferrall <nickoferrall@gmail.com>
2018-12-26 11:03:46 +0000

    Challenge using Match

--- sumOdd.js
+++ sumOdd.js
@@ -1,11 +1,20 @@
-function rowSumOddNumbers(n) {
-  let myAnswer = 0;
-  for (let i = 0; i < n.length; i++) {
-    if (n % 2 !== 0) {
-      myAnswer += n;
-    }
-  }
-  return myAnswer;
-}
+// function rowSumOddNumbers(n) {
+//   let myAnswer = 0;
+//   for (let i = 0; i < n.length; i++) {
+//     if (n % 2 !== 0) {
+//       myAnswer += n;
+//     }
+//   }
+//   return myAnswer;
+// }
 
-rowSumOddNumbers(3, 4, 5);
+// rowSumOddNumbers(3, 4, 5);
+
+const items = ["item1", "item2", "item3"];
+const copy = [];
+
+items.forEach(function(item) {
+  copy.push(item + "!");
+});
+
+console.log(copy);

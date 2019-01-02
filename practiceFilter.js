bf219839111a8280cfee1329ccfa107286e2c0fe
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-02 11:43:53 -0500

    Filter, map and reduce practice

---
+++ practiceFilter.js
@@ -0,0 +1,11 @@
+const array = [
+  'spray',
+  'limit',
+  'elite',
+  'exuberant',
+  'destruction',
+  'present'
+];
+
+const newArray = array.filter(i => i.length > 5);
+console.log(newArray);
---
+++ practiceMap.js
@@ -0,0 +1,5 @@
+console.log(['1', '2', '3'].map(str => parseInt(str)));
+
+const array1 = [1, 4, 9, 16];
+console.log(array1.map(i => i * 2));
+console.log('Original map array =', array1);

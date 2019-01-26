f9ab1204cc5781e2afffe6e621eef56d90671a75
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-26 18:56:26 -0500

    Cleaned challenge

--- sumOdd.js
+++ sumOdd.js
@@ -1,20 +1,8 @@
-// function rowSumOddNumbers(n) {
-//   let myAnswer = 0;
-//   for (let i = 0; i < n.length; i++) {
-//     if (n % 2 !== 0) {
-//       myAnswer += n;
-//     }
-//   }
-//   return myAnswer;
-// }
-
-// rowSumOddNumbers(3, 4, 5);
-
-const items = ["item1", "item2", "item3"];
+const items = ['item1', 'item2', 'item3'];
 const copy = [];
 
 items.forEach(function(item) {
-  copy.push(item + "!");
+  copy.push(item + '!');
 });
 
 console.log(copy);

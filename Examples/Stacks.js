5354ea01f5912df77eb7ffe80ade337473d551e8
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-02 11:37:20 -0500

    Added Stack example

---
+++ Examples/Stacks.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+let stack = new Stack();
+
+console.log(stack.push(180));
+console.log(stack.push(380));
+console.log(stack.last);
+console.log(stack.pop());
+console.log(stack.last);

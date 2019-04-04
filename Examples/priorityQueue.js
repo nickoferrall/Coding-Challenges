fb3b920b903a47c0cd9ae69c8d7d358b590d15ae
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-04 15:14:04 -0500

    Added heaps

--- Examples/priorityQueue.js
+++ Examples/priorityQueue.js
@@ -1,9 +1,10 @@
-class MaxBinaryHeap {
+class PriorityQueue {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [];
   }
-  insert(element) {
-    this.values.push(element);
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
     this.bubbleUp();
   }
   bubbleUp() {
@@ -12,21 +13,21 @@ class MaxBinaryHeap {
     while (idx > 0) {
       let parentIdx = Math.floor((idx - 1) / 2);
       let parent = this.values[parentIdx];
-      if (element <= parent) break;
+      if (element.priority >= parent.priority) break;
       this.values[parentIdx] = element;
       this.values[idx] = parent;
       idx = parentIdx;
     }
   }
-  extractMax() {
-    const max = this.values[0];
+  dequeue() {
+    const min = this.values[0];
     const end = this.values.pop();
     if (this.values.length > 0) {
       this.values[0] = end;
       // trickle down
       this.sinkDown();
     }
-    return max;
+    return min;
   }
   sinkDown() {
     let idx = 0;
@@ -40,15 +41,15 @@ class MaxBinaryHeap {
 
       if (leftChildIdx < length) {
         leftChild = this.values[leftChildIdx];
-        if (leftChild > element) {
+        if (leftChild.priority < element.priority) {
           swap = leftChildIdx;
         }
       }
       if (rightChildIdx < length) {
         rightChild = this.values[rightChildIdx];
         if (
-          (swap === null && rightChild > element) ||
-          (swap !== null && rightChild > leftChild)
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild < leftChild)
         ) {
           swap = rightChildIdx;
         }
@@ -61,13 +62,21 @@ class MaxBinaryHeap {
   }
 }
 
-let heap = new MaxBinaryHeap();
-heap.insert(55);
-// heap.insert(22);
-// heap.insert(25);
-// heap.insert(75);
-console.log(heap);
-console.log(heap.extractMax());
-console.log(heap);
-console.log(heap.bubbleUp());
-console.log(heap);
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+let ER = new PriorityQueue();
+ER.enqueue('Common cold', 5);
+ER.enqueue('Gunshot wound', 1);
+ER.enqueue('High fever', 4);
+ER.enqueue('Dengue', 2);
+ER.enqueue('ChickenG', 2);
+console.log('Er:', ER);
+console.log(ER.dequeue());
+console.log(ER.dequeue());
+console.log(ER.dequeue());
+console.log('Er:', ER);

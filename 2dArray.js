b02923687568b6740488628cfa99360e45403777
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-05-27 11:48:04 -0500

    Updated 2dArray with after new input

--- 2dArray.js
+++ 2dArray.js
@@ -33,45 +33,6 @@
 //   1
 // 8 6 6
 
-const arr = [
-  1,
-  1,
-  1,
-  0,
-  0,
-  0,
-  0,
-  1,
-  0,
-  0,
-  0,
-  0,
-  1,
-  1,
-  1,
-  0,
-  0,
-  0,
-  0,
-  9,
-  2,
-  -4,
-  -4,
-  0,
-  0,
-  0,
-  0,
-  -2,
-  0,
-  0,
-  0,
-  0,
-  -1,
-  -2,
-  -4,
-  0
-];
-
 // const arr = [
 //   1,
 //   1,
@@ -111,6 +72,24 @@ const arr = [
 //   0
 // ];
 
+// const arr = [
+//   [1, 1, 1, 0, 0, 0],
+//   [0, 1, 0, 0, 0, 0],
+//   [1, 1, 1, 0, 0, 0],
+//   [0, 0, 2, 4, 4, 0],
+//   [0, 0, 0, 2, 0, 0],
+//   [0, 0, 1, 2, 4, 0]
+// ];
+
+const arr = [
+  [-1, 1, -1, 0, 0, 0],
+  [0, -1, 0, 0, 0, 0],
+  [-1, -1, -1, 0, 0, 0],
+  [0, -9, 2, -4, -4, 0],
+  [-7, 0, 0, -2, 0, 0],
+  [0, 0, -1, -2, -4, 0]
+];
+
 // output = 19
 
 // have a count of the max hourglass total
@@ -124,26 +103,51 @@ const arr = [
 // i needs to be a multipler or + of j
 // if i is equal to the index to break to a new line, add more
 
+// function hourglassSum(arr) {
+//     let maxScore;
+//   //   for (let i = 0; i < arr.length - 14; i++) {
+//   //     let skipLine = [3, 9, 15];
+//   //     let currentScore =
+//   //       arr[i] +
+//   //       arr[i + 1] +
+//   //       arr[i + 2] +
+//   //       arr[i + 7] +
+//   //       arr[i + 12] +
+//   //       arr[i + 13] +
+//   //       arr[i + 14];
+//   //     if (!maxScore) {
+//   //       maxScore = currentScore;
+//   //     }
+//   //     if (currentScore > maxScore) {
+//   //       maxScore = currentScore;
+//   //     }
+//   //     if (skipLine.includes(i)) {
+//   //       i += 2;
+//   //     }
+//   //   }
+//   return arr[2][1];
+// }
+
+// get top row, middle and bottom row
+// outer for loop x4 depth. Inner x4 wide
+// top row = i, i+ 1, i + 2
+// middle = i + 1
+// bottom = i, i+ 1, i + 2
+
 function hourglassSum(arr) {
   let maxScore;
-  for (let i = 0; i < arr.length - 14; i++) {
-    let skipLine = [3, 9, 15];
-    let currentScore =
-      arr[i] +
-      arr[i + 1] +
-      arr[i + 2] +
-      arr[i + 7] +
-      arr[i + 12] +
-      arr[i + 13] +
-      arr[i + 14];
-    if (!maxScore) {
-      maxScore = currentScore;
-    }
-    if (currentScore > maxScore) {
-      maxScore = currentScore;
-    }
-    if (skipLine.includes(i)) {
-      i += 2;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let topRow = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+      let middleRow = arr[i + 1][j + 1];
+      let bottomRow = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+      let currentScore = topRow + middleRow + bottomRow;
+      if (maxScore == undefined) {
+        maxScore = currentScore;
+      }
+      if (maxScore < currentScore) {
+        maxScore = currentScore;
+      }
     }
   }
   return maxScore;

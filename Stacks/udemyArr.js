7bf3d3aa75a0991f50910777f1a3a83c74d783b6
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-06-03 11:18:23 -0500

    Stacks example with arr

---
+++ Stacks/udemyArr.js
@@ -0,0 +1,14 @@
+let stack = [];
+
+stack.push('Facebook');
+stack.push('Amazon');
+stack.push('Apple');
+stack.push('Netflix');
+stack.push('Google');
+
+stack.pop();
+stack.unshift('Alphabet');
+stack.shift();
+stack.shift();
+
+console.log(stack);

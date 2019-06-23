746994f38981e538f178378f56117627e24efcd0
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-06-22 19:29:21 -0500

    Votes

--- countingVotes.py
+++ countingVotes.py
@@ -19,15 +19,8 @@ def count_votes(arr):
     winners.sort()
     return winners[-1]
 
-# votes = [‘veronica’, ‘mary’, ‘alex’, ‘james’, ‘mary’, ‘michael’, ‘alex’, ‘michael’]
-
 
 votes = ['veronica', 'mary', 'alex', 'james',
          'mary', 'michael', 'alex', 'michael']
 
 print(count_votes(votes))
-
-
-# for name, age in dictionary.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
-#     if age == search_age:
-#         print(name)
